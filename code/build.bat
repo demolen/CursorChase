c59b4123521c2de9de76f8720072e6862b89e7d1
Milad Khodabandeh <xodabandeh@gmail.com>
2025-10-22 13:29:54 +0200

    IUI working

--- code/build.bat
+++ code/build.bat
@@ -3,13 +3,12 @@
 
 set CommonCompilerFlags= -std:c++latest -Zi -Wall -Wpedantic -fdiagnostics-absolute-paths -gcodeview -Wconversion -Wsign-conversion -Wnull-dereference -Wundef -Wno-missing-prototypes -Wno-language-extension-token -Wno-nested-anon-types -Wno-missing-field-initializers -Wno-gnu-anonymous-struct -Wno-unused-macros -Wno-old-style-cast -Wmissing-declarations -Wno-unused-variable -Wno-unused-parameter -Wno-unused-function -Wshadow -Od -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-undef -Wno-unsafe-buffer-usage -Wno-cast-function-type-strict -Wno-cast-function-type-mismatch
 
-set CommonLinkerFlags=user32.lib Shell32.lib
+set CommonLinkerFlags=user32.lib Shell32.lib Ole32.lib OleAut32.lib
 
 if not exist ..\build mkdir ..\build
 pushd ..\build
 
-clang-cl %CommonCompilerFlags%  ..\code\cursor.cpp /link /DLL /out:cursor.dll
+clang-cl %CommonCompil erFlags%  ..\code\cursor.cpp /link /DLL /out:cursor.dll
 clang-cl %CommonCompilerFlags%  ..\code\win32_cursor.cpp /link %CommonLinkerFlags%
-
 
 popd
--- misc/shell.bat
+++ misc/shell.bat
@@ -1,7 +1,7 @@
 :set VSINSTALLDIR=C:\Program Files\Microsoft Visual Studio\2022-17-16\Community\
 :set VCINSTALLDIR=C:\Program Files\Microsoft Visual Studio\2022-17-16\Community\VC\
-call "C:\Program Files (x86)\Microsoft GDK\Command Prompts\GamingDesktopVars.cmd" GamingDesktopVS2022 250401
-:call "C:\Program Files\Microsoft Visual Studio\2022-17-16\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+:call "C:\Program Files (x86)\Microsoft GDK\Command Prompts\GamingDesktopVars.cmd" GamingDesktopVS2022 250401
+call "C:\Program Files\Microsoft Visual Studio\2022-17-16\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 C:
 cd c:\ChaseTheCursor\code
 call "c:\ChaseTheCursor\misc\4coder\4ed.exe"
